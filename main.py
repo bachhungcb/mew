f72db0d8f7601d8e1b043d15de19dc7f1712d467
bachhungcb <81032588+bachhungcb@users.noreply.github.com>
2021-03-20 22:35:42 +0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,34 @@
+basic.show_leds("""
+    . . # . .
+    . . # # .
+    . . # . #
+    # # # . .
+    # # # . .
+    """)
+music.play_tone(392, music.beat(BeatFraction.HALF))
+music.play_tone(330, music.beat(BeatFraction.HALF))
+music.play_tone(392, music.beat(BeatFraction.HALF))
+music.play_tone(330, music.beat(BeatFraction.HALF))
+music.play_tone(392, music.beat(BeatFraction.HALF))
+music.play_tone(330, music.beat(BeatFraction.HALF))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(294, music.beat(BeatFraction.HALF))
+music.play_tone(349, music.beat(BeatFraction.HALF))
+music.play_tone(330, music.beat(BeatFraction.HALF))
+music.play_tone(294, music.beat(BeatFraction.HALF))
+music.play_tone(392, music.beat(BeatFraction.WHOLE))
+music.rest(music.beat(BeatFraction.SIXTEENTH))
+music.play_tone(392, music.beat(BeatFraction.HALF))
+music.play_tone(330, music.beat(BeatFraction.HALF))
+music.play_tone(392, music.beat(BeatFraction.HALF))
+music.play_tone(330, music.beat(BeatFraction.HALF))
+music.play_tone(392, music.beat(BeatFraction.HALF))
+music.play_tone(330, music.beat(BeatFraction.HALF))
+music.play_tone(262, music.beat(BeatFraction.WHOLE))
+music.play_tone(294, music.beat(BeatFraction.HALF))
+music.play_tone(349, music.beat(BeatFraction.HALF))
+music.play_tone(330, music.beat(BeatFraction.HALF))
+music.play_tone(294, music.beat(BeatFraction.HALF))
+music.play_tone(262, music.beat(BeatFraction.DOUBLE))
+music.play_tone(294, music.beat(BeatFraction.HALF))
+music.play_tone(294, music.beat(BeatFraction.HALF))
